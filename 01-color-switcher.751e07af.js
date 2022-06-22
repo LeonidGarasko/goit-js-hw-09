1182f8dc8bff20121ba244e6ffd16c43c0aa3ebe
LeonidGarasko <96500493+LeonidGarasko@users.noreply.github.com>
2022-06-22 17:21:12 +0000

    Deploying to gh-pages from @ LeonidGarasko/goit-js-hw-09@ce56d867f0c26526f854ca74ff7ed879b8bc6ccc 🚀

---
+++ 01-color-switcher.751e07af.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){timerIdd=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.setAttribute("disabled","disabled")})),e.addEventListener("click",(function(){clearInterval(timerIdd),t.removeAttribute("disabled","disabled")}))}();
+//# sourceMappingURL=01-color-switcher.751e07af.js.map
